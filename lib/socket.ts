1a04470bd3144345b950e2c38239a43b9e8dcc45
JeffWinder <info@jeffwinder.nl>
2021-12-09 08:32:25 +0100

    import Node's setTimeout & clearTimeout to prevent ambiguity

--- lib/socket.ts
+++ lib/socket.ts
@@ -3,6 +3,7 @@ import debugModule from "debug";
 import { IncomingMessage } from "http";
 import { Transport } from "./transport";
 import { Server } from "./server";
+import { setTimeout, clearTimeout } from "timers";
 
 const debug = debugModule("engine:socket");
 
@@ -222,7 +223,7 @@ export class Socket extends EventEmitter {
     // this function will manage packet events (also message callbacks)
     this.setupSendCallback();
 
-    this.cleanupFn.push(function() {
+    this.cleanupFn.push(function () {
       transport.removeListener("error", onError);
       transport.removeListener("packet", onPacket);
       transport.removeListener("drain", flush);
@@ -342,7 +343,7 @@ export class Socket extends EventEmitter {
     }
 
     // silence further transport errors and prevent uncaught exceptions
-    this.transport.on("error", function() {
+    this.transport.on("error", function () {
       debug("error triggered by discarded transport");
     });
 
